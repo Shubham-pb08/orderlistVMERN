fda83e45ec1e9948de8bcbe494cb8ef5bcfaf456
shubham-pb08 <shubhamsharma.pb08@gmail.com>
2025-01-01 20:12:38 -0500

    singup vercel

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -25,7 +25,7 @@ export const Signup = () => {
         e.preventDefault();
 
         try {
-            const response = await fetch('http://localhost:5000/api/signup',{
+            const response = await fetch('https://orderitems.vercel.app/api/signup',{
                 method: "post",
                 headers: {
                     "Content-Type": "application/json"
